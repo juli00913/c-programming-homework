e0e383dc4e8bf9e107f570679aad905bf0292342
juli00913 <jul.slavchev.tues19@gmail.com>
2016-10-26 23:37:18 +0300

    Create 01_task.c

---
+++ A/03/01_task.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, c, sum=0;
+    float avg;
+    printf("KOlko chisla:");
+    scanf("%d", &n);
+    for(i=0; n>i; i++) {
+        scanf("%d", &c);
+        sum= sum+c;
+    }
+    avg=sum;
+    avg=avg/i;
+    printf("Sum: %d\nAvg: %.2f", sum, avg);
+    return 0;
+}
